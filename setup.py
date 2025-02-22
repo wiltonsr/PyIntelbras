6004b7b7878b8be8dd1ea7d2663d1481a36851ae
Wilton Rodrigues <wiltonsr94@gmail.com>
2025-02-22 12:43:56 -0300

    Remove logging dependency

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with io.open('README.md', 'rt', encoding='utf8') as f:
 
 setup(
     name='pyintelbras',
-    version='0.0.23',
+    version='0.0.24',
     packages=['pyintelbras'],
     python_requires=">=3",
     url='https://github.com/wiltonsr/PyIntelbras',
@@ -27,7 +27,6 @@ setup(
     platforms='any',
     install_requires=[
         'requests',
-        'logging',
         'urllib3',
     ],
     classifiers=[
